ff8f601dbc6793994495a7ff71712ca56406038f
Robert Jackson <rjackson@linkedin.com>
2016-12-30 10:22:25 -0500

    Restructure into a monitor.
    The general structure here is completely stolen/copied
    from heimdalljs-fs-monitor.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,97 @@
+import heimdall from 'heimdalljs';
 import { Module } from 'module';
 
+// It is possible for this module to be evaluated more than once in the same
+// heimdall session. In that case, we need to guard against double-counting by
+// making other instances inert.
+let isMonitorRegistrant = false;
+let hasActiveInstance = false;
 let _load = null;
+
+class RequireSchema {
+  constructor() {
+    this.count = 0;
+    this.time = 0;
+    this.modules = [];
+  }
+
+  toJSON() {
+    return {
+      count: this.count,
+      time: this.time,
+      modules: this.modules
+    };
+  }
+}
+
+if (!heimdall.hasMonitor('require')) {
+  heimdall.registerMonitor('require', RequireSchema);
+  isMonitorRegistrant = true;
+}
 
 function nanosecondsSince(time) {
   let delta = process.hrtime(time);
   return delta[0] * 1e9 + delta[1];
 }
 
-export function instrument() {
+export default class RequireMonitor {
+  constructor() {
+    this.state = 'idle';
+  }
 
-}
+  start() {
+    if (isMonitorRegistrant && !hasActiveInstance) {
+      this.state = 'active';
+      this._attach();
+      hasActiveInstance = true;
+    } else {
 
-export function _installLoadWrapper(callback) {
-  _load = Module._load;
-  Module._load = function(requestedModuleId, parent) {
-    let start = process.hrtime();
+    }
+  }
 
-    let returnValue = _load.apply(Module, arguments);
+  stop() {
+    if (this.state === 'active') {
+      this._detach();
+      this.state = 'idle';
+      hasActiveInstance = false;
+    }
+  }
 
-    let info = {
-      requestedModuleId,
-      parentModuleId: parent.id,
-      duration: nanosecondsSince(start)
+  shouldMeasure() {
+    return this.state === 'active';
+  }
+
+  _detach() {
+    if (_load) {
+      Module._load = _load;
+    }
+  }
+
+  _attach() {
+    let monitor = this;
+    _load = Module._load;
+
+    Module._load = function (requestedModuleId, parent) {
+      if (monitor.shouldMeasure()) {
+        let metrics = heimdall.statsFor('require');
+        metrics.count++;
+        let start = process.hrtime();
+
+        let info = {
+          requestedModuleId,
+          parentModuleId: parent.id,
+          duration: null
+        };
+
+        metrics.modules.push(info);
+
+        let ret = _load.apply(Module, arguments);
+        let duration = info.duration = nanosecondsSince(start);
+        metrics.time += duration;
+        return ret;
+      }
+
+      return _load.apply(Module, arguments);
     };
-
-    callback(info);
-
-    return returnValue;
-  };
-}
-
-export function reset() {
-  if (_load) {
-    Module._load = _load;
   }
 }
--- tests/index.js
+++ tests/index.js
@@ -1,56 +1,90 @@
 import { Module } from 'module';
 import chai from 'chai';
-import Heimdall from 'heimdalljs/heimdall';
+import heimdall from 'heimdalljs';
 import clearRequire from 'clear-require';
 
-import {
-  _installLoadWrapper,
-  instrument,
-  reset
-} from '../src';
+import RequireMonitor from '../src';
 
 const { expect } = chai;
 const originalLoad = Module._load;
 
-describe('heimdalljs-require-instrumentation', function() {
-
+describe('heimdalljs-require-monitor', function() {
   beforeEach(function() {
     clearRequire('./fixtures/silly-module');
     clearRequire('./fixtures/slow-module');
   });
 
   afterEach(function() {
-    reset();
-    expect(Module._load, 'confirm reset works').to.equal(originalLoad);
+    expect(Module._load, 'Module._load has been reset').to.equal(originalLoad);
   });
 
-  describe('._installLoadWrapper', function() {
-    it('callback receives module information', function() {
-      let info;
-      _installLoadWrapper((_info) => {
-        info = _info;
+  it('will only allow one active instance at a time', function() {
+    let monitor0 = new RequireMonitor();
+    let monitor1 = new RequireMonitor();
+
+    try {
+      monitor0.start();
+      monitor1.start();
+
+      expect(monitor0.state, 'monitor0 (m0 active)').to.eql('active');
+      expect(monitor1.state, 'monitor1 (m0 active)').to.eql('idle');
+
+      monitor0.stop();
+
+      monitor1.start();
+      monitor0.start();
+
+      expect(monitor0.state, 'monitor0 (m1 active)').to.eql('idle');
+      expect(monitor1.state, 'monitor1 (m1 active)').to.eql('active');
+
+    } finally {
+      monitor0.stop();
+      monitor1.stop();
+    }
+  });
+
+  describe('adds stats', function() {
+    let token, monitor;
+
+    beforeEach(function() {
+      token = heimdall.start('test harness');
+    });
+
+    afterEach(function() {
+      if (token) {
+        // stop in a way compat with 0.2 and 0.3
+        heimdall.stop ? heimdall.stop(token) : token.stop();
+      }
+
+      if (monitor) {
+        monitor.stop();
+      }
+    });
+
+    it('does not log stats unless started', function() {
+      let stats = heimdall.statsFor('require');
+      require('./fixtures/silly-module');
+      expect(stats).to.deep.equal({
+        time: 0,
+        count: 0,
+        modules: []
       });
+    });
+
+    it('logs stats when started', function() {
+      let stats = heimdall.statsFor('require');
+      monitor = new RequireMonitor();
+      monitor.start();
 
       require('./fixtures/slow-module');
 
-      expect(info.requestedModuleId).to.equal('./fixtures/slow-module');
-      expect(info.parentModuleId).to.equal(__filename);
-      expect(info.duration).to.be.above(5000000);
-    });
+      expect(stats.count).to.equal(1);
+      expect(stats.modules.length).to.equal(1);
 
-    it('does not break `require`ing files', function() {
-      let info;
-      _installLoadWrapper((_info) => {
-        info = _info;
-      });
-
-      let ret = require('./fixtures/silly-module');
-
-      expect(ret).to.deep.equal({
-        foo: 'foo',
-        bar: 'bar',
-        derp: ['a', 'b', 'c']
-      });
+      let moduleInfo = stats.modules[0];
+      expect(moduleInfo.parentModuleId).to.equal(__filename);
+      expect(moduleInfo.requestedModuleId).to.equal('./fixtures/slow-module');
+      expect(moduleInfo.duration).to.be.above(5000000);
     });
   });
 });
